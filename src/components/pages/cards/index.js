6fabee40570229d67e0c8ef0bef7a89707929b54
Smartwebcbe <hostingsmartweb@gmail.com>
2022-11-20 19:18:26 +0530

    Day 4: passing Props manually, Passing a function getclickcount as Props to display Total Clicks

--- src/components/pages/cards/index.js
+++ src/components/pages/cards/index.js
@@ -1,17 +1,18 @@
 import React from "react";
 
-export class Cards extends React.Component{
-    render(){
-        return(
+export class Cards extends React.Component {
+
+    render() {
+        return (
             <div>
                 <div class="card">
-  <img src="https://cdn4.buysellads.net/uu/1/127574/1668535585-SST41.jpg" class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+                    <img src="https://cdn4.buysellads.net/uu/1/127574/1668535585-SST41.jpg" class="card-img-top" alt="..." />
+                    <div class="card-body">
+                        <h5 class="card-title">{this.props.title}</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary" onClick={this.props.clickcount}>Click Here</a>
+                    </div>
+                </div>
             </div>
         )
     }
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,32 +1,40 @@
 import React from "react";
-import {Cards} from "../../components/pages/cards";
+import { Cards } from "../../components/pages/cards";
 
-export class Home extends React.Component{
+export class Home extends React.Component {
 
-    state={
-        text:""
+    state = {
+        text: "",
+        count: 0
     }
 
-    handlePrint=(event)=>{
+    handlePrint = (event) => {
         console.log(this.state.text);
-        this.setState({text:"Vijay Anand"});
+        this.setState({ text: "Vijay Anand" });
     }
+    getclickcount = () => {
+        this.setState({ count: this.state.count + 1 })
+    }
+
     // handleAlert=()=>{
     //     alert("User added");
     // }
-    render(){
-        return(
-           <div class="container mt-5">
-            {/* <h1>HOME PAGE</h1> */}
-            
-           <div>
+    render() {
+        return (
+            <div class="container mt-5">
+                {/* <h1>HOME PAGE</h1> */}
+
+                {/* <div>
            <button type="button" class="btn btn-primary" onClick={this.handlePrint}>Print Text</button>
             <h4>Printed Text: {this.state.text}</h4>
-           </div>
-            <div class="row">
-<div class="col-4"><Cards/></div>
+           </div> */}
+                <h4>Click count: {this.state.count}</h4>
+                <div class="row">
+                    <div class="col-4"><Cards title="User 01" clickcount={this.getclickcount} /></div>
+                    <div class="col-4"><Cards title="User 02" clickcount={this.getclickcount} /></div>
+                    <div class="col-4"><Cards title="User 03" clickcount={this.getclickcount} /></div>
+                </div>
             </div>
-           </div>
         );
     }
 }
